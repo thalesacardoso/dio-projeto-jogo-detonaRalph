784686ca5356f2de983336b122ff165a0303acd3
thalesacardoso <thales.dev@hotmail.com>
2023-10-05 13:39:29 -0300

    jogo finalizado

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,12 +1,8 @@
 /*
 DESENVOLVIDO POR: Thales Cardoso
 GITHUB: https://github.com/thalesacardoso
-DATA: 
+DATA: 05/10/2023
 */
-
-function print(txt) {
-   console.log(txt)
-}
 
 const states = {
    view: {
@@ -18,9 +14,9 @@ const states = {
       button: document.getElementById('btn-continuar')
    },
    values: {
-      timeTotal: 5,
+      timeTotal: 30,
       scoreTotal: 0,
-      lifeTotal: 5,
+      lifeTotal: 3,
       position: 0,
    },
    action: {
@@ -29,29 +25,63 @@ const states = {
    }
 };
 
+// Função que reinicia o jogo
+function reiniciarJogo() {
+   // Configura o novo valor de tempo total
+   states.values.timeTotal = 30;
+   states.action.contador = setInterval(mudarTempo, 1000)
+   states.action.enemy = setInterval(randomEnemy, 1000)
+   mudarVida()
+}
 
-function mudarVida(){
+// Função que toca um som quando acerta o inimigo no jogo
+function tocarSom(nomeAudio) {
+   let som = new Audio(`./src/audios/${nomeAudio}.m4a`);
+   som.volume = 0.2
+   som.play()
+}
+
+// Função que altera a vida no jogo
+function mudarVida() {
    states.view.life.innerText = states.values.lifeTotal--
    let vida = states.view.life.innerText
+   let pontos = states.view.score.innerText
+   let contador = states.view.time.innerText
+   let btn = states.view.button
 
-   if(vida <= 0){
-      print('Total de vidas '+ vida)
+   if (vida <= 0 && contador <= 0) {
+      alert(`Você marcou ${pontos} pontos no total.\n Reinicie a página para tentar novamente`)
+
+      // Remove o botão de continuar
+      btn.classList.remove('ativo')
+
+      // Mantém a vida zerada até reiniciar a página
       states.view.life.innerText = 0;
+
+      // Para a execução do SetInterval quando o contador chega a Zero
+      clearInterval(states.action.contador)
+      clearInterval(states.action.enemy)
    }
 }
 
 // Função que altera o tempo no jogo
 function mudarTempo() {
-      states.view.time.innerText = states.values.timeTotal--
-      let contador = states.view.time.innerText
+   states.view.time.innerText = states.values.timeTotal--
+   let contador = states.view.time.innerText
+   let btn = states.view.button
+   btn.classList.remove('ativo')
 
-      // Verifica se o contador chegou a zero
-      if(contador <= 0) {
-         print('Game Over')
-         // Para a execução da função SetInterval quando o contador chega a Zero
-         clearInterval(states.action.contador)
-         clearInterval(states.action.enemy)
-      }
+   // Verifica se o contador chegou a zero
+   if (contador <= 0) {
+
+      // Mostra o botão continuar
+      btn.classList.add('ativo')
+
+      // alert('Game Over')
+      // Para a execução do SetInterval quando o contador chega a Zero
+      clearInterval(states.action.contador)
+      clearInterval(states.action.enemy)
+   }
 }
 
 // Função que cria o movimento do inimigo de forma aleatória
@@ -79,7 +109,7 @@ function addListenerHitBox() {
       square.addEventListener('mousedown', () => {
          // verifica se o id do click é igual a posição que o inimigo se encontra na tela
          if (square.id == states.values.position) {
-            
+
             // soma 1 ponto no total do score
             states.values.scoreTotal++
 
@@ -88,15 +118,18 @@ function addListenerHitBox() {
 
             // reseta a posição do inimigo para que não tenha como ficar clicando e somando pontos no mesmo lugar
             states.values.position = null;
+
+            tocarSom('hit')
          }
       })
    })
 
 }
 
+// Função principal autoexecutável
 function main() {
+   alert('ACERTE O ALVO PARA MARCAR PONTOS')
    addListenerHitBox()
-   mudarVida()
 }
 
 main()
