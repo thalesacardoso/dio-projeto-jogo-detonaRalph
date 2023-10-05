0abbc601c56bfea8c04ab3113a8693d5b033c853
thalesacardoso <thales.dev@hotmail.com>
2023-10-05 10:12:27 -0300

    Mudança de tempo do jogo completo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -14,21 +14,47 @@ const states = {
       score: document.getElementById('score'),
       life: document.getElementById('life'),
       square: document.querySelectorAll('.square'),
-      enemy: document.querySelector('.enemy')
+      enemy: document.querySelector('.enemy'),
+      button: document.getElementById('btn-continuar')
    },
    values: {
-      time: 60,
-      timerID: 0,
-      score: 0,
-      life: 3,
-      speed: 500,
+      timeTotal: 5,
+      scoreTotal: 0,
+      lifeTotal: 5,
       position: 0,
    },
-   action: {}
+   action: {
+      contador: setInterval(mudarTempo, 1000),
+      enemy: setInterval(randomEnemy, 1000),
+   }
 };
 
 
+function mudarVida(){
+   states.view.life.innerText = states.values.lifeTotal--
+   let vida = states.view.life.innerText
 
+   if(vida <= 0){
+      print('Total de vidas '+ vida)
+      states.view.life.innerText = 0;
+   }
+}
+
+// Função que altera o tempo no jogo
+function mudarTempo() {
+      states.view.time.innerText = states.values.timeTotal--
+      let contador = states.view.time.innerText
+
+      // Verifica se o contador chegou a zero
+      if(contador <= 0) {
+         print('Game Over')
+         // Para a execução da função SetInterval quando o contador chega a Zero
+         clearInterval(states.action.contador)
+         clearInterval(states.action.enemy)
+      }
+}
+
+// Função que cria o movimento do inimigo de forma aleatória
 function randomEnemy() {
    // remove a classe enemy de dentro de todos os squares
    states.view.square.forEach((values) => {
@@ -46,20 +72,20 @@ function randomEnemy() {
    states.values.position = enemy.id
 }
 
-function moveEnemy(){
-   states.values.timerID = setInterval(randomEnemy,states.values.speed)
-}
-
+// Função que faz marcar os pontos com o click do mouse
 function addListenerHitBox() {
 
    states.view.square.forEach((square) => {
       square.addEventListener('mousedown', () => {
          // verifica se o id do click é igual a posição que o inimigo se encontra na tela
-         if (square.id == states.values.position){
+         if (square.id == states.values.position) {
+            
             // soma 1 ponto no total do score
-            states.values.score++
+            states.values.scoreTotal++
+
             // altera o valor do html com o valor total do score
-            states.view.score.textContent = states.values.score
+            states.view.score.textContent = states.values.scoreTotal
+
             // reseta a posição do inimigo para que não tenha como ficar clicando e somando pontos no mesmo lugar
             states.values.position = null;
          }
@@ -69,8 +95,8 @@ function addListenerHitBox() {
 }
 
 function main() {
-   moveEnemy()
    addListenerHitBox()
+   mudarVida()
 }
 
 main()
